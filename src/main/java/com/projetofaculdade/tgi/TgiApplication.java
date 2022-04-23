3a3ec4c01b0f86879336f4c0ea1681003faa02a3
Danilo <dcassianodias@gmail.com>
2022-04-23 14:59:42 -0300

    Project created

---
+++ src/main/java/com/projetofaculdade/tgi/TgiApplication.java
@@ -0,0 +1,13 @@
+package com.projetofaculdade.tgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetofaculdade/tgi/TgiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetofaculdade.tgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
